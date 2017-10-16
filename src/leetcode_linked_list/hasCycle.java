87f7e4fcc92715456f5630dede2be7bc9c54a5cd
HeatDeath <632668308@qq.com>
2017-10-16 23:46:59 +0800

    add

--- src/leetcode_linked_list/hasCycle.java
+++ src/leetcode_linked_list/hasCycle.java
@@ -1,5 +1,19 @@
 package leetcode_linked_list;
 
-public class hasCycle {
+import java.awt.List;
 
+public class hasCycle {
+	public boolean hasCycle(ListNode head) {
+		ListNode pre = head;
+		ListNode p = head;
+		while (p != null && p.next != null) {
+			if (p.next == head) {
+				return true;
+			}
+			p = p.next;
+			pre.next = head;
+			pre = p;
+		}
+		return false;
+	}
 }
--- src/leetcode_linked_list/mergeTwoLists.java
+++ src/leetcode_linked_list/mergeTwoLists.java
@@ -1,5 +1,25 @@
 package leetcode_linked_list;
 
 public class mergeTwoLists {
-
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode head = new ListNode(0);
+		ListNode l3 = head;
+		while (l1 != null && l2 != null) {
+			if (l1.val > l2.val) {
+				l3.next = l2;
+				l2 = l2.next;
+			}else {
+				l3.next = l1;
+				l1 = l1.next;
+			}
+            l3 = l3.next;
+		}
+		
+		if(l2 == null){
+	        l3.next = l1;
+	    } else {
+	        l3.next = l2;
+	    }
+		return head.next;
+	}
 }
