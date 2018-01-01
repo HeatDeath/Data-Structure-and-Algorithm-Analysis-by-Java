b2e2b7ead81a5fc986b4ae6e19746488b77f533c
HeatDeath <632668308@qq.com>
2018-01-01 23:28:01 +0800

    add

---
+++ src/com/concurrent_programming/IockInterruptiblyTest.java
@@ -0,0 +1,57 @@
+package com.concurrent_programming;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class IockInterruptiblyTest {
+    private Lock lock = new ReentrantLock();
+    public static void main(String[] args)  {
+        IockInterruptiblyTest test = new IockInterruptiblyTest();
+        MyThread_1 thread1 = new MyThread_1(test);
+        MyThread_1 thread2 = new MyThread_1(test);
+        thread1.start();
+        thread2.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread2.interrupt();
+    }
+
+    public void insert(Thread thread) throws InterruptedException{
+        lock.lockInterruptibly();   //注意，如果需要正确中断等待锁的线程，必须将获取锁放在外面，然后将InterruptedException抛出
+        try {
+            System.out.println(thread.getName()+"得到了锁");
+            long startTime = System.currentTimeMillis();
+            for(    ;     ;) {
+                if(System.currentTimeMillis() - startTime >= 20000)
+                    break;
+                //插入数据
+            }
+        }
+        finally {
+            System.out.println(Thread.currentThread().getName()+"执行finally");
+            lock.unlock();
+            System.out.println(thread.getName()+"释放了锁");
+        }
+    }
+
+}
+
+class MyThread_1 extends Thread {
+    private IockInterruptiblyTest test = null;
+    public MyThread_1(IockInterruptiblyTest test) {
+        this.test = test;
+    }
+    @Override
+    public void run() {
+
+        try {
+            test.insert(Thread.currentThread());
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName()+"被中断");
+        }
+    }
+}
---
+++ src/com/concurrent_programming/ReentrantReadWriteLockTest_1.java
@@ -0,0 +1,31 @@
+package com.concurrent_programming;
+
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReentrantReadWriteLockTest_1 {
+    private ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    public static void main(String[] args) {
+        final ReentrantReadWriteLockTest_1 test = new ReentrantReadWriteLockTest_1();
+        new Thread() {
+            public void run() {
+                test.get(Thread.currentThread());
+            }
+        }.start();
+
+        new Thread() {
+            public void run() {
+                test.get(Thread.currentThread());
+            }
+        }.start();
+    }
+
+    public synchronized void get(Thread thread) {
+        long start = System.currentTimeMillis();
+        while (System.currentTimeMillis() - start <= 1) {
+            System.out.println(thread.getName() + "正在进行读操作");
+        }
+        System.out.println(thread.getName() + "读操作完毕");
+    }
+
+}
